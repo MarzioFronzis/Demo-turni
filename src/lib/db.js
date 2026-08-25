37ba1355c7f48bb2b94536ec33ce5581a2ca7a05
MarzioFronzis <andrea.marzio.marziali@gmail.com>
2026-08-25 17:24:28 +0200

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -9,35 +9,36 @@ import {
   writeBatch,
 } from "firebase/firestore";
 import { db } from "./firebase";
+import { COLL } from "./collections";
 
 // ---------- Skill ----------
-export const aggiungiSkill = (nome) => addDoc(collection(db, "skills"), { nome });
-export const eliminaSkill = (id) => deleteDoc(doc(db, "skills", id));
+export const aggiungiSkill = (nome) => addDoc(collection(db, COLL.skills), { nome });
+export const eliminaSkill = (id) => deleteDoc(doc(db, COLL.skills, id));
 
 // ---------- Dipendenti ----------
-export const aggiungiDipendente = (dip) => addDoc(collection(db, "dipendenti"), dip);
-export const aggiornaDipendente = (id, campi) => updateDoc(doc(db, "dipendenti", id), campi);
-export const eliminaDipendente = (id) => deleteDoc(doc(db, "dipendenti", id));
+export const aggiungiDipendente = (dip) => addDoc(collection(db, COLL.dipendenti), dip);
+export const aggiornaDipendente = (id, campi) => updateDoc(doc(db, COLL.dipendenti, id), campi);
+export const eliminaDipendente = (id) => deleteDoc(doc(db, COLL.dipendenti, id));
 
 export async function aggiungiAssenza(dipendenteId, dipendenteAttuale, assenza) {
   const nuove = [...(dipendenteAttuale.assenze || []), { ...assenza, id: `a${Date.now()}` }];
-  return updateDoc(doc(db, "dipendenti", dipendenteId), { assenze: nuove });
+  return updateDoc(doc(db, COLL.dipendenti, dipendenteId), { assenze: nuove });
 }
 export async function eliminaAssenza(dipendenteId, dipendenteAttuale, assenzaId) {
   const nuove = (dipendenteAttuale.assenze || []).filter((a) => a.id !== assenzaId);
-  return updateDoc(doc(db, "dipendenti", dipendenteId), { assenze: nuove });
+  return updateDoc(doc(db, COLL.dipendenti, dipendenteId), { assenze: nuove });
 }
 
 // ---------- Fabbisogno ----------
-export const aggiungiFascia = (fascia) => addDoc(collection(db, "fabbisogno"), fascia);
-export const aggiornaFascia = (id, campi) => updateDoc(doc(db, "fabbisogno", id), campi);
-export const eliminaFascia = (id) => deleteDoc(doc(db, "fabbisogno", id));
+export const aggiungiFascia = (fascia) => addDoc(collection(db, COLL.fabbisogno), fascia);
+export const aggiornaFascia = (id, campi) => updateDoc(doc(db, COLL.fabbisogno, id), campi);
+export const eliminaFascia = (id) => deleteDoc(doc(db, COLL.fabbisogno, id));
 
 // Scrittura in blocco (usata da clona giorno / clona settimana ricorrente)
 export async function aggiungiFasceInBlocco(fasce) {
   const batch = writeBatch(db);
   fasce.forEach((f) => {
-    const ref = doc(collection(db, "fabbisogno"));
+    const ref = doc(collection(db, COLL.fabbisogno));
     batch.set(ref, f);
   });
   await batch.commit();
@@ -45,12 +46,12 @@ export async function aggiungiFasceInBlocco(fasce) {
 
 // ---------- Priorità (un documento per skill, id documento = skillId) ----------
 export const salvaPriorita = (skillId, ordineDipendentiIds) =>
-  setDoc(doc(db, "priorita", skillId), { ordine: ordineDipendentiIds });
+  setDoc(doc(db, COLL.priorita, skillId), { ordine: ordineDipendentiIds });
 
 // ---------- Proposte (un documento per settimana, id = "2026-W50") ----------
-export const salvaProposta = (chiaveSettimana, proposta) => setDoc(doc(db, "proposte", chiaveSettimana), proposta);
+export const salvaProposta = (chiaveSettimana, proposta) => setDoc(doc(db, COLL.proposte, chiaveSettimana), proposta);
 
 export async function leggiProposta(chiaveSettimana) {
-  const snap = await getDoc(doc(db, "proposte", chiaveSettimana));
+  const snap = await getDoc(doc(db, COLL.proposte, chiaveSettimana));
   return snap.exists() ? snap.data() : null;
 }
