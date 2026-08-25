e7aa2729f83fd750ba441d6387f63925202de478
MarzioFronzis <andrea.marzio.marziali@gmail.com>
2026-08-25 17:23:54 +0200

    Create collections.js

---
+++ src/lib/collections.js
@@ -0,0 +1,9 @@
+// Prefisso dedicato per non entrare in conflitto con le collection dell'altra app
+// (turni-app) che condivide lo stesso progetto Firebase.
+export const COLL = {
+  skills: "motoreturni_skills",
+  dipendenti: "motoreturni_dipendenti",
+  fabbisogno: "motoreturni_fabbisogno",
+  priorita: "motoreturni_priorita",
+  proposte: "motoreturni_proposte",
+};
